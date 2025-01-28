6c22215f3164d1d19fa4d77811ff8fa53d40e40a
manoel <piloukos45@gmail.com>
2025-01-28 16:40:28 -0300

    test

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,40 +1,29 @@
-import { createContext, useContext, useState} from "react";
-import { signInWithCustomToken } from "firebase/auth"
-import { useNavigate } from "react-router-dom";;
+import { createContext, useContext, useEffect, useState } from "react";
+import { signInWithCustomToken } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase/firebase";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-
   const navigate = useNavigate();
 
   const getAuthenticated = localStorage.getItem("Authenticated");
-  const [authenticated, setAuthenticated] = useState(getAuthenticated || null);
-  // const [currentUser, setCurrentUser] = useState(null);
-
+  const [authenticated, setAuthenticated] = useState(getAuthenticated || false);
+    
   const urlParams = new URLSearchParams(window.location.search);
   const token = urlParams.get("token") || localStorage.getItem("token");
-  
+  localStorage.setItem("token", token);
+
 
   if (token) {
-      
-      signInWithCustomToken(auth, token)
-          .then((userCredential) => {
-              const user = userCredential.user;
-              console.log("Usuário autenticado com sucesso:", user);
-              localStorage.setItem("token", token);
-              localStorage.setItem("Authenticated", true);
-              localStorage.setItem("userUid", JSON.stringify(user.uid));
-          })
-          .catch((error) => {
-              console.error("Erro ao autenticar:", error.message, error.code);
-              localStorage.removeItem("token"); // Limpa o token inválido
-              navigate("/login"); // Redireciona para a página de login
-          });
+    console.log("Token encontrado, tentando autenticar...");
+
+    // logica para validar o token 
+
   } else {
-      console.warn("Nenhum token encontrado. Redirecionando para login.");
-      navigate("/login");
+    console.log("Nenhum token encontrado");
+    navigate("/login");
   }
 
   return (
@@ -51,3 +40,4 @@ export const useAuthContext = () => {
   }
   return context;
 };
+
