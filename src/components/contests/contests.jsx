3bc8251908e4ca6872a3451e37a8d319ee5e6157
hrm4k3r <hebertdev82@gmail.com>
2025-01-30 01:08:56 -0300

    feat: create card from contest

--- src/components/contests/contests.jsx
+++ src/components/contests/contests.jsx
@@ -5,15 +5,33 @@ const Contests = () => {
     <Contests_style>
       <div className="deals">
         <div className="card">
-          Concurso 1
+          <p>BOLÃO DE SÁBADO</p>
+          <p>Todo Sábado, começa às 21h</p>
+          <hr />
+          <p>R$ 200.000,00 MIL</p>
+          <p>6 prêmios</p>
+          <hr />
+          <a href="#">APOSTE AGORA</a>
         </div>
 
         <div className="card">
-          Concurso 2
+          <p>BOLÃO DE SEGUNDA</p>
+          <p>Todo Segunda, começa às 21h</p>
+          <hr />
+          <p>R$ 45.000,00 MIL</p>
+          <p>5 prêmios</p>
+          <hr />
+          <a href="#">APOSTE AGORA</a>
         </div>
 
         <div className="card">
-          Concurso 3
+          <p>BOLÃO DE QUARTA</p>
+          <p>Todo Quarta, começa às 21h</p>
+          <hr />
+          <p>R$ 86.800,00 MIL</p>
+          <p>5 prêmios</p>
+          <hr />
+          <a href="#">APOSTE AGORA</a>
         </div>
       </div>
     </Contests_style>
