e8b932b45802702f4c2bfe9a68fe4a57adb63d38
hrm4k3r <hebertdev82@gmail.com>
2025-01-30 01:42:04 -0300

    feat: create card from contest

--- src/components/contests/contests.jsx
+++ src/components/contests/contests.jsx
@@ -1,22 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import { db } from '../../firebase/firebase';
+import { collection, getDocs } from 'firebase/firestore';
 import { Contests_style } from "./contestsStyle";
 
 const Contests = () => {
+  const [jogos, setJogos] = useState([]);
+  const [limite, setLimite] = useState(6);
+
+  async function getJogos() {
+    const jogosCollection = collection(db, "jogos");
+    const jogosSnapshot = await getDocs(jogosCollection);
+    const jogosList = jogosSnapshot.docs.map(doc => doc.data());
+    setJogos(jogosList);
+  }
+
+  useEffect(() => {
+    getJogos();
+  }, []);
+
+  const handleSetLimit = () => {
+    if (limite >= jogos.length) {
+      setLimite(jogos.length);
+      return
+    }
+    setLimite(limite + 3);
+  };
+
   return (
     <Contests_style>
       <div className="deals">
-        <div className="card">
-          <h2>BOLÃO DE SÁBADO</h2>
-          <p className="details">Todo Sábado, começa às 21h</p>
-          <hr />
-          <p className="details prize">R$ 200.000,00 MIL</p>
-          <p className="details">6 prêmios</p>
 
-          <div className="button">
-            <a href="#">APOSTE AGORA</a>
+        {jogos.slice(0, limite).map((jogo, index) => (
+          <div className="card">
+            <h2>{jogo.title || 'Nome do Jogo'} </h2>
+            <p className="details">Todo Sábado, começa às 21h</p>
+            <hr />
+            <p className="details prize">R$ 200.000,00 MIL</p>
+            <p className="details">6 prêmios</p>
+
+            <div className="button">
+              <a href="#">APOSTE AGORA</a>
+            </div>
+
+            <span>Apenas R$ 5,00</span>
           </div>
-
-          <span>Apenas R$ 5,00</span>
-        </div>
+        ))}
 
         <div className="card">
           <h2>BOLÃO DE SEGUNDA</h2>
