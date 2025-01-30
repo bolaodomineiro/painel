efeda4e2a94bb0e43b78755cceda85dfdc729c62
hrm4k3r <hebertdev82@gmail.com>
2025-01-30 01:44:35 -0300

    feat: create card from contest

--- src/components/contests/contests.jsx
+++ src/components/contests/contests.jsx
@@ -29,50 +29,21 @@ const Contests = () => {
   return (
     <Contests_style>
       <div className="deals">
-
         {jogos.slice(0, limite).map((jogo, index) => (
           <div className="card">
             <h2>{jogo.title || 'Nome do Jogo'} </h2>
-            <p className="details">Todo Sábado, começa às 21h</p>
+            <p className="details">{jogo.data || 'Data do Sorteio'}</p>
             <hr />
-            <p className="details prize">R$ 200.000,00 MIL</p>
-            <p className="details">6 prêmios</p>
+            <p className="details prize">{jogo.cotas || 'Cotas não disponíveis'}</p>
+            <p className="details">{jogo.description || 'Descrição não disponível'}</p>
 
             <div className="button">
               <a href="#">APOSTE AGORA</a>
             </div>
 
-            <span>Apenas R$ 5,00</span>
+            <span>{jogo.price || 'Preço não disponível'}</span>
           </div>
         ))}
-
-        <div className="card">
-          <h2>BOLÃO DE SEGUNDA</h2>
-          <p className="details">Todo Segunda, começa às 21h</p>
-          <hr />
-          <p className="details prize">R$ 45.000,00 MIL</p>
-          <p className="details">5 prêmios</p>
-
-          <div className="button">
-            <a href="#">APOSTE AGORA</a>
-          </div>
-
-          <span>Apenas R$ 5,00</span>
-        </div>
-
-        <div className="card">
-          <h2>BOLÃO DE QUARTA</h2>
-          <p className="details">Todo Quarta, começa às 21h</p>
-          <hr />
-          <p className="details prize">R$ 86.800,00 MIL</p>
-          <p className="details">5 prêmios</p>
-
-          <div className="button">
-            <a href="#">APOSTE AGORA</a>
-          </div>
-
-          <span>Apenas R$ 5,00</span>
-        </div>
       </div>
     </Contests_style>
   );
