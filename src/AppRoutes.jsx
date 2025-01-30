5dd304fb8124311f9a8a438fd8b201db8a0949e1
hrm4k3r <hebertdev82@gmail.com>
2025-01-30 00:29:27 -0300

    feat: create contests item

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -2,16 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Users from './pages/users/Users';
-import Contests from './pages/contests/Contests';
 import Pages from './pages/pagesEdit/Pages';
 import Components from './pages/componentsEdit/Components';
 
-function AppRoutes({ $menuToggle}) {
+function AppRoutes({ $menuToggle }) {
     return (
         <Routes>
             <Route path="/dashboard" element={<Home />} />
-            <Route path="/users" element={<Users  $menuToggle={$menuToggle} />} />
-            <Route path="/contests" element={<Contests />} />
+            <Route path="/users" element={<Users $menuToggle={$menuToggle} />} />
             <Route path="/pages" element={<Pages />} />
             <Route path="/components" element={<Components />} />
         </Routes>
