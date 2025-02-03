14b4ca897b88f35aafb4ffc2dc6b989b0e9f5cc5
manoel <piloukos45@gmail.com>
2025-02-03 10:27:39 -0300

    test

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -9,61 +9,70 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [message, setMessage] = useState(null);
   const navigate = useNavigate();
-
   const [authenticated, setAuthenticated] = useState(false);
   const [loading, setLoading] = useState(true);
   const secretKey = "sua-chave-secreta";
 
   useEffect(() => {
     const verifyUser = async () => {
-      try {
-        // Obtém o UID da URL ou localStorage
-        const urlParams = new URLSearchParams(window.location.search);
-        const urlUid = urlParams.get("uid");
-        let encryptedUid = urlUid ? decodeURIComponent(urlUid) : localStorage.getItem("userId");
+      // try {
+      //   // Obtém o UID da URL ou localStorage
+      //   const urlParams = new URLSearchParams(window.location.search);
+      //   const urlUid = urlParams.get("uid");
+      //   console.log("UID da URL:", urlUid);
+      //   let encryptedUid = urlUid ? decodeURIComponent(urlUid) : decodeURIComponent(localStorage.getItem("userId"));
 
-        if (!encryptedUid) throw new Error("Nenhum UID encontrado.");
+      //   if (!encryptedUid) throw new Error("Nenhum UID encontrado.");
 
-        // Descriptografa o UID
-        const decryptedBytes = CryptoJS.AES.decrypt(encryptedUid, secretKey);
-        const decryptedUid = decryptedBytes.toString(CryptoJS.enc.Utf8);
+      //   // Descriptografa o UID
+      //   const decryptedBytes = CryptoJS.AES.decrypt(encryptedUid, secretKey);
+      //   const decryptedUid = decryptedBytes.toString(CryptoJS.enc.Utf8);
 
-        if (!decryptedUid) throw new Error("UID descriptografado é inválido.");
+      //   if (!decryptedUid) throw new Error("UID descriptografado é inválido.");
 
-        // Armazena o UID descriptografado no localStorage
-        localStorage.setItem("userId", decryptedUid);
+      //   // Armazena o UID descriptografado no localStorage
+      //   localStorage.setItem("userId", urlUid);
 
-        // Busca o documento do usuário no Firestore
-        const userDocRef = doc(db, "users", decryptedUid);
-        const userDocSnap = await getDoc(userDocRef);
+      //   // Busca o documento do usuário no Firestore
+      //   const userDocRef = doc(db, "users", decryptedUid);
+      //   const userDocSnap = await getDoc(userDocRef);
 
-        if (!userDocSnap.exists()) throw new Error("Usuário não encontrado.");
+      //   if (!userDocSnap.exists()) throw new Error("Usuário não encontrado.");
 
-        const userData = userDocSnap.data();
-        const storedToken = userData.token;
+      //   const userData = userDocSnap.data();
+      //   const storedToken = userData.token;
 
-        if (!storedToken) throw new Error("Nenhum token encontrado no Banco de Dados.");
+      //   if (!storedToken) throw new Error("Nenhum token encontrado no Banco de Dados.");
 
-        const user = auth.currentUser;
-        console.log(user);
-        if (!user) throw new Error("Usuário não autenticado.");
+      //   // Checa a autenticação do usuário no Firebase
+      //   const user = auth.currentUser;
+      //   if (!user) throw new Error("Usuário não autenticado.");
 
-        const tokenResult = await user.getIdTokenResult(true);
-        const now = Date.now() / 1000;
+      //   // Verifica o token do Firebase
+      //   const tokenResult = await user.getIdTokenResult(true);
+      //   const now = Date.now() / 1000;
 
-        if (tokenResult.expirationTime < now) throw new Error("Token expirado. Entre com seus dados novamente.");
+      //   if (tokenResult.expirationTime < now) throw new Error("Token expirado. Entre com seus dados novamente.");
 
-        // Usuário autenticado com sucesso
-        setAuthenticated(true);
-        localStorage.setItem("authenticated", "true"); // opcional, dependendo do seu fluxo
+      //   // Comparar o token do Firestore com o token do Firebase
+      //   if (storedToken !== tokenResult.token) {
+      //     throw new Error("Token inválido.");
+      //   }
 
-      } catch (error) {
-        console.error("Erro ao autenticar usuário:", error.message);
-        setMessage(error.message); // Armazenando a mensagem de erro
-        navigate("/error"); // Redireciona para uma página de erro
-      } finally {
-        setLoading(false);
-      }
+      //   // Usuário autenticado com sucesso
+      //   setAuthenticated(true);
+      //   localStorage.setItem("authenticated", "true"); // Opcional, dependendo do seu fluxo
+
+      //   // Redireciona para a página inicial ou qualquer outra página desejada
+      //   navigate("/dashboard/jogo");
+
+      // } catch (error) {
+      //   console.error("Erro ao autenticar usuário:", error.message);
+      //   setMessage(error.message); // Armazenando a mensagem de erro
+      //   navigate("/error"); // Redireciona para uma página de erro (se necessário)
+      // } finally {
+      //   setLoading(false);
+      // }
     };
 
     verifyUser();
